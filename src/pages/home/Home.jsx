7bda22c1cbe690f17ee47f91a2af10cad06b0889
Yeasin Munshi <142327064+yeasin5060@users.noreply.github.com>
2024-04-04 10:55:22 +0600

    image_folder_add

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Homebaner from './homebaner/Homebaner'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <Homebaner/>
+    </>
   )
 }
 
---
+++ src/pages/home/homebaner/Homebaner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Homebaner = () => {
+  return (
+   <section className ="pt-[39px] pb-[36px] ">
+        hello
+   </section>
+  )
+}
+
+export default Homebaner
