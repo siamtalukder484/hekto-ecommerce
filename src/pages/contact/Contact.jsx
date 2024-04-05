71e750349101a696f672fdfe44a7bec6585ccdc9
km-saifullah <kmsaifullah16@gmail.com>
2024-04-05 11:40:05 +0600

    login page done

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -9,12 +9,14 @@ const Contact = () => {
     <section>
       <div className="bg-[#F6F5FF] py-[100px]">
         <div className="max-w-container mx-auto">
-          <h3 className="font-Josefin text-4xl leading-normal font-bold pb-2 text-[#101750]">
-            Contact Us
-          </h3>
-          <p className="text-black font-lato text-base font-medium leading-normal">
-            Home.Pages.<span className="text-btn-color">Contact us</span>
-          </p>
+          <div className="pl-[71px]">
+            <h3 className="font-Josefin text-4xl leading-normal font-bold pb-2 text-[#101750]">
+              Contact Us
+            </h3>
+            <p className="text-black font-lato text-base font-medium leading-normal">
+              Home.Pages.<span className="text-btn-color">Contact us</span>
+            </p>
+          </div>
         </div>
       </div>
       <div className="max-w-container mx-auto">
