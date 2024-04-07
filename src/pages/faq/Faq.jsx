0127064a0b1e0c0d948e4500e89442fbb5511e28
Modabbir Mossen <125083879+w3modabbir@users.noreply.github.com>
2024-04-07 12:22:37 +0600

    faq done

--- src/pages/faq/Faq.jsx
+++ src/pages/faq/Faq.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PeraGrap from '../../utilities/PeraGrap'
 
 const Faq = () => {
   return (
@@ -16,7 +17,33 @@ const Faq = () => {
           </div>
         </div>
       </div>
-      <div></div>
+      <div className='max-w-container mx-auto'>
+        <div className='flex py-[100px]'>
+          <div className=''>
+            <h2 className='text-4xl font-Josefin font-bold capitalize text-heading-color pb-[60px]'>Generel Information</h2>
+
+            <div className='flex flex-col gap-[65px]'>
+              <div>
+                <h3 className='text-2xl font-Josefin font-semibold capitalize text-heading-color pb-[15px]'>Eu dictumst cum at sed euismood condimentum?</h3>
+                <PeraGrap style="text-pera-color font-lato font-normal text-sm max-w-[470px] leading-6" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt sed tristique mollis vitae, consequat gravida sagittis."/>
+              </div>
+              <div>
+                <h3 className='text-2xl font-Josefin font-semibold capitalize text-heading-color pb-[15px]'>Magna bibendum est fermentum eros.</h3>
+                <PeraGrap style="text-pera-color font-lato font-normal text-sm max-w-[470px] leading-6" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt sed tristique mollis vitae, consequat gravida sagittis."/>
+              </div>
+              <div>
+                <h3 className='text-2xl font-Josefin font-semibold capitalize text-heading-color pb-[15px]'>Odio muskana hak eris conseekin sceleton?</h3>
+                <PeraGrap style="text-pera-color font-lato font-normal text-sm max-w-[470px] leading-6" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt sed tristique mollis vitae, consequat gravida sagittis."/>
+              </div>
+              <div>
+                <h3 className='text-2xl font-Josefin font-semibold capitalize text-heading-color pb-[15px]'>Elit id blandit sabara boi velit gua mara?</h3>
+                <PeraGrap style="text-pera-color font-lato font-normal text-sm max-w-[470px] leading-6" text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt sed tristique mollis vitae, consequat gravida sagittis."/>
+              </div>
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
     </div>
   )
 }
